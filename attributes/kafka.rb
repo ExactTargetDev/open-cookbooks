1c31c40614510eb37ba7a42e04db8f089455a216
dkardach <dkardach@ais.pl>
2011-07-18 11:00:31 +0200

    Upgrade to Kafka 0.6.

--- attributes/kafka.rb
+++ attributes/kafka.rb
@@ -1,7 +1,7 @@
 default[:kafka][:zookeeper_boxes] = 1
 default[:kafka][:zookeeper_role] = 'zookeeper_server'
 
-default[:kafka][:distribution] = 'kafka-0.05'
+default[:kafka][:distribution] = 'kafka-0.6'
 default[:kafka][:source] = 'http://sna-projects.com/kafka/downloads/'
 
 default[:kafka][:logrotate_freq] = 'daily'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,19 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "/tmp/".concat(node[:kafka][:distribution]).concat(".tar.gz") do
-  source node[:kafka][:source].concat(node[:kafka][:distribution]).concat(".tar.gz")
+remote_file "/tmp/".concat(node[:kafka][:distribution]).concat(".zip") do
+  source node[:kafka][:source].concat(node[:kafka][:distribution]).concat(".zip")
   mode "755"
   action :create_if_missing
 end
 
 execute "Untar Kafka" do
-  command "cd /tmp/;tar -zxvf ".concat(node[:kafka][:distribution]).concat(".tar.gz")
+  command "unzip -qo /tmp/#{node[:kafka][:distribution]}.zip -d /tmp/"
   action :run
 end
 
 execute "Copy Kafka to installation dir." do
-  command "cp -fr /tmp/".concat(node[:kafka][:distribution]).concat(" /usr/local")
+  command "cp -fr /tmp/kafka /usr/local"
   action :run
 end
 
@@ -37,32 +37,37 @@ logrotate_app "kafka" do
   rotate node[:kafka][:logrotate_rotate]
 end
 
-zookeeper_pairs = []
-loop_count = 10
-boxes = node[:kafka][:zookeeper_boxes]
-print "Waiting for all ZooKeeper instances...\n"
-while loop_count > 0
-  zookeeper_pairs = []
-  search(:node, "run_list:role\\[#{node[:kafka][:zookeeper_role]}\\] AND chef_environment:#{node.chef_environment}") do |zkn|
-    zookeeper_pairs << "#{zkn.fqdn}:2181"
-  end
-
-  print zookeeper_pairs.length.to_s.concat(" of ").concat(boxes.to_s).concat(" available...\n")
-  if zookeeper_pairs.length == boxes then
-    break
-  end
-  sleep 3 * loop_count
-  loop_count -= 1
+zookeeper_pairs = wait_for("Zookeeper", node[:kafka][:zookeeper_boxes], 10) { fqdns_for_role("#{node[:kafka][:zookeeper_role]}") }
+i = 0
+while i < zookeeper_pairs.size do
+  zookeeper_pairs[i] = zookeeper_pairs[i].concat(":2181")
+  i += 1
 end
+#loop_count = 10
+#boxes = node[:kafka][:zookeeper_boxes]
+#print "Waiting for all ZooKeeper instances...\n"
+#while loop_count > 0
+#  zookeeper_pairs = []
+#  search(:node, "run_list:role\\[#{node[:kafka][:zookeeper_role]}\\] AND chef_environment:#{node.chef_environment}") do |zkn|
+#    zookeeper_pairs << "#{zkn.fqdn}:2181"
+#  end
 
-template "/usr/local/".concat(node[:kafka][:distribution]).concat("/config/server.properties") do
+#  print zookeeper_pairs.length.to_s.concat(" of ").concat(boxes.to_s).concat(" available...\n")
+#  if zookeeper_pairs.length == boxes then
+#    break
+#  end
+#  sleep 3 * loop_count
+#  loop_count -= 1
+#end
+
+template "/usr/local/kafka/config/server.properties" do
   variables(
     :zookeeper_pairs => zookeeper_pairs
   )
   source "server.properties.erb"
 end
 
-template "/usr/local/".concat(node[:kafka][:distribution]).concat("/bin/kafka-run-class.sh") do
+template "/usr/local/kafka/bin/kafka-run-class.sh" do
   source "kafka-run-class.sh.erb"
   mode 0755
 end
